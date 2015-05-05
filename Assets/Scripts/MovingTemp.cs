c4390bc6ac4e0e78f692dd78c977731765e0c809
ArminPlohl <arminplohl@gmail.com>
2015-05-05 21:54:11 +0200

    Added environment stuff.

---
+++ Assets/Scripts/MovingTemp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingTemp : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * 2 * Time.deltaTime);
+        //Debug.Log("Moving!");
+    }
+}
