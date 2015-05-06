f24dcb51f3be3bf7f426477c6dec263a37215d86
ArminPlohl <arminplohl@gmail.com>
2015-05-06 23:55:35 +0200

    Added bg animation. Implemented game over menu.

---
+++ Assets/HUDConroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDConroller : MonoBehaviour
+{
+    public MenuScript script;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onButtonClick()
+    {
+        script.HideMenu();
+    }
+}
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+    Animator animator;
+
+    int showHash, hideHash;
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        showHash = Animator.StringToHash("Show");
+        hideHash = Animator.StringToHash("Hide");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetTrigger(showHash);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetTrigger(hideHash);
+        }
+    }
+
+    public void HideMenu()
+    {
+        animator.SetTrigger(hideHash);
+    }
+}
