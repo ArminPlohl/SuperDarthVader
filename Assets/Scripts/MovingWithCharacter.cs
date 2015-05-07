a24390a73b4e35b563bdf010873539f474e2f4d6
jernejk <jernej.kavka@windowslive.com>
2015-05-07 21:47:45 +0200

    Moved stuff

---
+++ Assets/Scripts/MovingWithCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MovingWithCharacter : MonoBehaviour
+{
+    public GameObject CharacterObject;
+    public float DistanceFromCharacter = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        // Spremenimo le X os kamere
+        position.x = CharacterObject.transform.position.x + DistanceFromCharacter;
+
+        transform.position = position;
+
+        // Ker vemo y in z kamere, lahko tudi tako napišemo:
+        //transform.position = new Vector3(transform.position.x + DistanceFromCharacter, 0, -10);
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject[] Objects;
+    public float MinimumSpawnTime = 1f;
+    public float MaxiumSpawnTime = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        GameObject obj = Instantiate<GameObject>(Objects[Random.Range(0, Objects.Length)]);
+
+        Vector3 position = transform.position;
+
+        position.y += Mathf.RoundToInt(Random.Range(0, 3)) * 3;
+
+        obj.transform.position = position;
+        
+        Invoke("Spawn", Random.Range(MinimumSpawnTime, MaxiumSpawnTime));
+    }
+}
